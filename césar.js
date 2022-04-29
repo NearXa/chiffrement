a4578b2d94e2a7f960071090a6f966d4e7109fe2
NearXa <104648362+NearXa@users.noreply.github.com>
2022-04-29 20:20:32 +0200

    Create césar.js

---
+++ césar.js
@@ -0,0 +1,17 @@
+function Ceasar(word, offset) {
+    let newArray = [];
+    for (let i = 0; i < word.length; i++) {
+        if (word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) {
+            newArray.push((((word.charCodeAt(i) - 65) + offset) % 26) + 65);
+        }
+        else {
+            newArray.push((((word.charCodeAt(i) - 97) + offset) % 26) + 97);
+        }
+    }
+    let newWords = "";
+    for (let j = 0; j < newArray.length; j++) {
+        newWords += String.fromCharCode(newArray[j])
+    }
+    return newWords;
+}
+console.log(Ceasar("Boule", 3)) 
